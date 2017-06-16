c8e8eb504826b9581872875fd175ce18fb97080c
hx <781742009@qq.com>
2017-06-16 16:41:59 +0800

    Initial Commit

---
+++ read/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  read
+//
+//  Created by 20161104601 on 17/6/16.
+//  Copyright © 2017年 20161104601. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
