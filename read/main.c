ac48d41763908fe5efb522d302a142248ec0b5d6
hx <781742009@qq.com>
2017-06-16 18:00:35 +0800

    gps
    gps从文件中读出前两行。

--- read/main.c
+++ read/main.c
@@ -7,9 +7,26 @@
 //
 
 #include <stdio.h>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+int main()
+{
+    FILE *fp ;
+    char str[1000];
+    int max;
+    int player, score, timein, round;
+    fp=fopen("TXT", "r" ); //注意调整文件名
+    if( fp==NULL )
+    {
+        printf("open file erorr\n");
+        return -1;
+    }
+    fgets(str, sizeof(str), fp ); //读第一行
+    sscanf(str,"%d", &max );
+    while( fgets(str, sizeof(str), fp )) //读其余行
+    {
+        sscanf(str, "%d%d%d%d", &player, &round, &timein, &score ); //从字符串读取相关数据
+        printf("玩家： %02d  得分： %3d  上场时间： %5d 场次： %02d\n",
+               player, score, timein, round );
+    }
+    fclose(fp);
     return 0;
 }
